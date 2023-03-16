2ace7f1700c32d529b061060480ca39a6263ac08
user <user@Debian-KevinPan>
2023-03-16 18:04:13 +0800

    前後端分離

--- PCA9684_32Servo_ESP32.h
+++ PCA9684_32Servo_ESP32.h
@@ -1,57 +1,5 @@
 const char pageHeader_p1[] PROGMEM = R"header_p1(
-<!DOCTYPE html>
-<html><head>
-  <title>Robojax PCA9685 ESP32 Servo Control</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <script>
-    const d = document,
-          qry  = (...x) => d.querySelector(...x);
-          qrys = (...x) => d.querySelectorAll(...x);
-
-    addEventListener("load", ()=>{
-      qry("#allServo").addEventListener("click", e => {
-        if(e.innerText == "Start All Servo"){
-          fetch("servo?do=start");
-          e.innerHTML = "Stop All Servo";
-        } else {
-          fetch("servo?do=stop");
-          e.innerHTML = "Start All Servo";
-        }
-      });
-      qrys("#ctrl td.servo").forEach(s => {
-        const [disp, decBtn, input, incBtn] = [...s.children];
-        decBtn.addEventListener("click", ()=>{
-          input.value--;
-          input.dispatchEvent(new Event("change"));
-        });
-        incBtn.addEventListener("click", ()=>{
-          input.value++;
-          input.dispatchEvent(new Event("change"));
-        });
-        input.addEventListener("change", e => {
-          disp.innerHTML = "" + input.value;
-          fetch("servo?" + new URLSearchParams({
-            servo: s.dataset.servo,
-            deg: input.value
-          }));
-        })
-      });
-    });
-  </script>
-  <style>
-    * { box-sizing: border-box }
-    td.servo span {
-      display: inline-block;
-      min-width: 4ch;
-      text-align: center;
-    }
-  </style>
-</head><body>
-  <h1>Robojax PCA9685 ESP32 Servo</h1>
 )header_p1";
-
-
-////////////////for motor 1 part 1///////////////
 const char servoControl_p1[] PROGMEM = R"body_p1(
 
 
--- robot.ino
+++ robot.ino
@@ -11,8 +11,8 @@ V5 video of PCA9685 32 Servo with ESP32 with WiFi https://youtu.be/bvqfv-FrrLM
 V4 video of PCA9685 32 Servo with ESP32 (no WiFi): https://youtu.be/JFdXB8Za5Os
 V3 video of PCA9685 how to control 32 Servo motors https://youtu.be/6P21wG7N6t4
 V2 Video of PCA9685 3 different ways to control Servo motors: https://youtu.be/bal2STaoQ1M
-V1 Video introduction to PCA9685 to control 16 Servo  https://youtu.be/y8X9X10Tn1k
- *  
+V1 Video introduction to PCA9685 to control 16 Servo	https://youtu.be/y8X9X10Tn1k
+ *	
  * Written by Ahmad Shamshiri for Robojax Video channel www.Robojax.com
  * Date: Jan 25, 2021, in Ajax, Ontario, Canada
 
@@ -23,29 +23,30 @@ Learn Arduino step by step in structured course with all material, wiring diagra
 all in once place. Purchase My course on Udemy.com http://robojax.com/L/?id=62
 
 ****************************
-Get early access to my videos via Patreon and have  your name mentioned at end of very 
+Get early access to my videos via Patreon and have	your name mentioned at end of very 
 videos I publish on YouTube here: http://robojax.com/L/?id=63 (watch until end of this video to list of my Patrons)
 ****************************
 
 or make donation using PayPal http://robojax.com/L/?id=64
 
- *  * This code is "AS IS" without warranty or liability. Free to be used as long as you keep this note intact.* 
+ *	* This code is "AS IS" without warranty or liability. Free to be used as long as you keep this note intact.* 
  * This code has been download from Robojax.com
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+		This program is free software: you can redistribute it and/or modify
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation, either version 3 of the License, or
+		(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+		GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+		You should have received a copy of the GNU General Public License
+		along with this program.	If not, see <https://www.gnu.org/licenses/>.
  */
 ////////////////////// PCA9685 settings started
-#include <Wire.h>
+
+// #include <Wire.h>
 
 #include <Adafruit_PWMServoDriver.h>
 
@@ -59,8 +60,8 @@ int maximumServo = 32; //how many servos are connected
 // for max range. You'll have to tweak them as necessary to match the servos you
 // have!
 // Watch video V1 to understand the two lines below: http://youtu.be/y8X9X10Tn1k
-#define SERVOMIN  125 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  575 // this is the 'maximum' pulse length count (out of 4096)
+#define SERVOMIN	125 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX	575 // this is the 'maximum' pulse length count (out of 4096)
 
 int servoAngle = 0;
 int servoStep = 10;
@@ -69,34 +70,34 @@ int stepDelay = 50; // 50 milliseconds
 int servoAngleMin = 0;
 int servoAngleMax = 180;
 
-// minimum angle of each servo     
+// minimum angle of each servo		 
 bool servoShown[] = {
-  0, 1, 1, 1, 1, 1, 1, 1,
-  1, 1, 0, 0, 0, 0, 0, 0,
-  1, 1, 1, 1, 1, 1, 1, 1,
-  1, 0, 0, 0, 0, 0, 0, 0,
+	0, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 0, 0, 0, 0, 0, 0,
+	1, 1, 1, 1, 1, 1, 1, 1,
+	1, 0, 0, 0, 0, 0, 0, 0,
 };
 
 int allServoMin[]={
-  0,    0,    0,    0,    0,    0,    0,    0,  //  1 to  8
-  0,    0,    0,    0,    0,    0,    0,    0,  //  9 to 16
-  0,    0,    0,    0,    0,    0,    0,    0,  // 17 to 24
-  0,    0,    0,    0,    0,    0,    0,    0   // 25 to 32                   
+	0, 0, 0, 0, 0, 0, 0, 0, //	1 to  8
+	0, 0, 0, 0, 0, 0, 0, 0, //	9 to 16
+	0, 0, 0, 0, 0, 0, 0, 0, // 17 to 24
+	0, 0, 0, 0, 0, 0, 0, 0  // 25 to 32										
 }; 
 //maximum value of each servo
 
 int allServoMax[] = {
-  180,    180,    180,    180,    180,    180,    180,    180,  //  1 to  8 
-  180,    180,    180,    180,    180,    180,    180,    180,  //  9 to 16
-  180,    180,    180,    180,    180,    180,    180,    180,  // 17 to 24 
-  180,    180,    180,    180,    180,    180,    180,    180   // 25 to 32 
+	180, 180, 180, 180, 180, 180, 180, 180,	//	1 to  8 
+	180, 180, 180, 180, 180, 180, 180, 180,	//	9 to 16
+	180, 180, 180, 180, 180, 180, 180, 180,	// 17 to 24 
+	180, 180, 180, 180, 180, 180, 180, 180  // 25 to 32 
 }; 
-// initial position of servos      
+// initial position of servos			 
 int allServoPosition[] = {
-  0,    0,    0,    0,    0,    0,    0,    0,  //  1 to  8
-  0,    0,    0,    0,    0,    0,    0,    0,  //  9 to 16
-  0,    0,    0,    0,    0,    0,    0,    0,  // 17 to 24
-  0,    0,    0,    0,    0,    0,    0,    0   // 25 to 32               
+	0, 0, 0, 0, 0, 0, 0, 0,	//	1 to  8
+	0, 0, 0, 0, 0, 0, 0, 0,	//	9 to 16
+	0, 0, 0, 0, 0, 0, 0, 0,	// 17 to 24
+	0, 0, 0, 0, 0, 0, 0, 0  // 25 to 32								
 };
 int servoNumber = 100; //servo to move
 int buttonPushed = 0;
@@ -107,9 +108,9 @@ int angleToPulse(int ang); //this is prototype of function defined at the end of
 ////////////////////////PCA9685 ended
 
 #include "PCA9684_32Servo_ESP32.h"
-  
+	
 #include <WiFi.h>
-#include <WiFiClient.h>
+// #include <WiFiClient.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
 
@@ -120,143 +121,139 @@ WebServer server(80);
 
 const int led = 13;
 
-/////////////////////////////////////
-void handleRoot() {
-  // Watch video explanation for this https://youtu.be/bvqfv-FrrLM
-  String HTML_page = pageHeader_p1; 
+void handleInfo() {
+	String minStr = "", maxStr = "", shownStr = "", posStr;
+		// turn arrays allServoMin, allServoMax, servoShown, and allServoPosition into string
 
-  char tmp[200] = "";
-  sprintf(tmp, R"html(
-    <button id="allServo">%s All Servo</button>
-  )html", allServo ? "Stop" : "Start");
+	if(maximumServo > 0){ // the first is special: `${thing}`
+		minStr.concat(allServoMin[0]);
+		maxStr.concat(allServoMax[0]);
+		shownStr.concat(servoShown[0] ? "true" : "false");
+		posStr.concat(allServoPosition[0]);
+	}
+	for(int i = 1; i < maximumServo; i++){ // others are in this pattern: `, ${thing}`
+		minStr.concat(",");
+		maxStr.concat(",");
+		shownStr.concat(",");
+		posStr.concat(",");
 
-  HTML_page.concat(tmp);
+		minStr.concat(allServoMin[i]);
+		maxStr.concat(allServoMax[i]);
+		shownStr.concat(servoShown[i] ? "true" : "false");
+		posStr.concat(allServoPosition[i]);
+	}
 
-  if(allServo) {
-    HTML_page.concat("<div class=\"btn\"><a class=\"angleButton colorAll\"  href=\"/servo?do=stop\">Stop Servo</a></div>");  
-  }else{
-    HTML_page.concat("<div class=\"btn\"><a class=\"angleButton colorAll\"  href=\"/servo?do=all\">All Servo</a></div>");    
-  }
-
-  HTML_page.concat(R"html(
-  <table id="ctrl">
-    <th>#</th><th>Deg</th>
-  )html");
-  for (int i = 0; i < maximumServo; i++) {
-    char tmp[500] = "";
-    sprintf(tmp, R"html(
-      <tr>
-        <td>%d</td>
-        <td class="servo" data-servo="%d">
-          <span>%d</span>
-          <input type="button" value="-" />
-          <input type="range" max="%d" min="%d" value="%d" />
-          <input type="button" value="+" />
-        </td>
-      </tr>
-    )html", i, i, allServoPosition[i], allServoMax[i], allServoMin[i], allServoPosition[i]);
-    HTML_page.concat(servoShown[i] ? tmp : "");
-  }
-  HTML_page.concat("</table>");
-  
-  HTML_page.concat("</body></html>");
-  server.send(200, "text/html", HTML_page);
+	char info[minStr.length() + maxStr.length() + shownStr.length() + 200] = "";
+	sprintf(info, R"json({
+		"success": true,
+		"data": {
+			"num": %d,
+			"runningAll": %s,
+			"min": [%s],
+			"max": [%s],
+			"shown": [%s],
+			"pos": [%s]
+		}
+	})json", maximumServo, allServo ? "true": "false", minStr.c_str(), maxStr.c_str(), shownStr.c_str(), posStr.c_str());
+	server.send(200, "application/json", info);
 }
 
 void handleNotFound() {
-  digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
+	digitalWrite(led, 1);
 
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
+	String message = "File Not Found\n\n";
+	message += "URI: ";
+	message += server.uri();
+	message += "\nMethod: ";
+	message += (server.method() == HTTP_GET) ? "GET" : "POST";
+	message += "\nArguments: ";
+	message += server.args();
+	message += "\n";
 
-  server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
+	for (uint8_t i = 0; i < server.args(); i++) {
+		message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+	}
+
+	server.send(404, "text/plain", message);
+	digitalWrite(led, 0);
 }
 
 
 void setup() {
 	//Watch video explanation for this https://youtu.be/bvqfv-FrrLM
-  board1.begin();
-  board2.begin();  
-  board1.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
-  board2.setPWMFreq(60);
+	board1.begin();
+	board2.begin();  
+	board1.setPWMFreq(60);	// Analog servos run at ~60 Hz updates
+	board2.setPWMFreq(60);
 
-  //initial position of all servos
-  for(int i = 0; i < maximumServo; i++) {
-    if(i < 16) {
-      board1.setPWM(i   , 0, angleToPulse(allServoPosition[i]) ); 
-    } else {
-      board2.setPWM(i-15, 0, angleToPulse(allServoPosition[i]) );       
-    }
-  }
-    Serial.begin(115200);
-    Serial.println("32 channel Servo test!");
+	//initial position of all servos
+	for(int i = 0; i < maximumServo; i++) {
+		if(i < 16) {
+			board1.setPWM(i	  , 0, angleToPulse(allServoPosition[i]) ); 
+		} else {
+			board2.setPWM(i-15, 0, angleToPulse(allServoPosition[i]) );				
+		}
+	}
+		Serial.begin(115200);
+		Serial.println("32 channel Servo test!");
 
-  // Servo control using ESP32 from Robojax.com
+	// Servo control using ESP32 from Robojax.com
 
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("");
-    
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, password);
+	Serial.println("");
+		
+	// Wait for connection
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
 
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+	Serial.println("");
+	Serial.print("Connected to ");
+	Serial.println(ssid);
+	Serial.print("IP address: ");
+	Serial.println(WiFi.localIP());
 
 
-  if (MDNS.begin("robojaxESP32")) {
-    Serial.print("MDNS responder started at http://");
-    Serial.println("robojaxESP32");
-  }
+	if (MDNS.begin("robojaxESP32")) {
+		Serial.print("MDNS responder started at http://");
+		Serial.println("robojaxESP32");
+	}
 
-  server.enableCORS();
-  server.on("/", handleRoot);
-  server.on("/servo", HTTP_GET, handleServo); 
+	server.enableCORS();
+	server.on("/", [](){ server.send(200, "text/html", "Please open index.html in your browser"); });
+	server.on("/servo", HTTP_GET, handleServo); 
+	server.on("/info", handleInfo);
 
-  server.onNotFound(handleNotFound);
-  server.begin();
-  Serial.println("HTTP server started"); 	
+	server.onNotFound(handleNotFound);
+	server.begin();
+	Serial.println("HTTP server started");	
 }
 
 void loop() {
 	server.handleClient();
-  if(allServo){
-    for(int angle = servoAngleMin; angle <= servoAngleMax; angle += servoStep){
-      for(int i = 0; i < 16; i++) {      
-            board2.setPWM(i, 0, angleToPulse(angle));
-            board1.setPWM(i, 0, angleToPulse(angle));
-      }
-      delay(stepDelay);
-    }
-    // robojax PCA9865 32 channel Servo control
-    delay(100);        
-  }
-  
-  // if pushed    
-  if(buttonPushed && 0 <= servoNumber && servoNumber < maximumServo){
-    if(servoNumber < 16) {
-      board1.setPWM( servoNumber   , 0, angleToPulse(allServoPosition[servoNumber]) ); 
-    } else {
-      board2.setPWM( servoNumber-15, 0, angleToPulse(allServoPosition[servoNumber]) );       
-    }
-  }
-  buttonPushed = 0; 
+	if(allServo){
+		for(int angle = servoAngleMin; angle <= servoAngleMax; angle += servoStep){
+			for(int i = 0; i < 16; i++) {			 
+						board2.setPWM(i, 0, angleToPulse(angle));
+						board1.setPWM(i, 0, angleToPulse(angle));
+			}
+			delay(stepDelay);
+		}
+		// robojax PCA9865 32 channel Servo control
+		delay(100);				 
+	}
+	
+	// if pushed		
+	if(buttonPushed && 0 <= servoNumber && servoNumber < maximumServo){
+		if(servoNumber < 16) {
+			board1.setPWM( servoNumber   , 0, angleToPulse(allServoPosition[servoNumber]) ); 
+		} else {
+			board2.setPWM( servoNumber-15, 0, angleToPulse(allServoPosition[servoNumber]) );			 
+		}
+	}
+	buttonPushed = 0; 
 }
 
 
@@ -271,21 +268,21 @@ void loop() {
  */
 void handleServo() {
 	// Watch video explanation for this https://youtu.be/bvqfv-FrrLM
-  allServo = server.arg("do") == "all" ? 1 : 0;
+	allServo = server.arg("do") == "all" ? 1 : 0;
 
-  int argServo = server.arg("servo").toInt(),
-      argDeg = server.arg("deg").toInt();
-  if(
-    0 <= argServo && argServo < maximumServo &&
-    allServoMin[argServo] <= argDeg && argDeg <= allServoMax[argServo]
-  ) {
-    allServoPosition[argServo] = argDeg; 
-    servoNumber = argServo;
+	int argServo = server.arg("servo").toInt(),
+	    argDeg = server.arg("deg").toInt();
+	if(
+		0 <= argServo && argServo < maximumServo &&
+		allServoMin[argServo] <= argDeg && argDeg <= allServoMax[argServo]
+	) {
+		allServoPosition[argServo] = argDeg; 
+		servoNumber = argServo;
 
-    buttonPushed = 1;
-  }
-  // handleRoot();
-  server.send(200, "text/html", "Finished.");
+		buttonPushed = 1;
+	}
+	// handleRoot();
+	server.send(200, "text/html", "Finished.");
 }
 
 /*
@@ -295,6 +292,6 @@ void handleServo() {
  * written by Ahmad Shamshiri for Robojax, Robojax.com
  */
 int angleToPulse(int ang) {
-   int pulse = map(ang, 0, 180, SERVOMIN, SERVOMAX);// map angle of 0 to 180 to Servo min and Servo max
-   return pulse;
+	 int pulse = map(ang, 0, 180, SERVOMIN, SERVOMAX);// map angle of 0 to 180 to Servo min and Servo max
+	 return pulse;
 }
